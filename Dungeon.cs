3db4a401e2ad9e4e6eae114f4419e5c7dc2ffdb7
ViolinRobot <isaacsiebelink@gmail.com>
2019-10-30 17:40:46 -0700

    Will be getting rid of Room.cs eventually

--- Dungeon.cs
+++ Dungeon.cs
@@ -1,31 +1,60 @@
 ﻿using System;
 
 namespace Generator {
-	public class Room {
-    	bool floor;
-	    bool walls;
-    	bool ceiling;
-	    bool floor;
-    	string shape;
-	    string lighting;
-    	// Size size;
-	    // Portal[] portals;
+	public class Generator {
 
-    	public Room() { 
-    	}
+		public void GenerateLayout() {
+		}
 
-	    // Reuturn some sort of graphical layout
-    	void GenerateLayout() {
+		public struct Room {
+			bool floor;
+			bool walls;
+			bool ceiling;
+			string shape;
+			string lighting;
+			Size size;
+			Portal[] portals;
 
-    	}
-	}
+			public Room(bool theFloor=true, bool theWalls=true, bool theCeiling=true, string theShape="rectangle", string theLighting="light", Size theSize, Portal[] thePortals) {
+				floor = theFloor;
+				walls = theWalls;
+				ceiling = theCeiling;
+				shape = theShape;
+				lighting = theLighting;
+				size = theSize;
+				portals = thePortals;
+			}			
+		}
 
-	public struct Portal {
-		public Room roomA, roomB;
+		public struct Portal {
+			public Room roomA, roomB;
 
-		public Portal() {
-			roomA = room1;
-			roomB = room2;	
+			public Portal(Room room1, Room room2) {
+				roomA = room1;
+				roomB = room2;	
+			}
+		}
+
+		public struct Hall {
+			public string lighting;
+			public Portal portalA, portalB;
+			public Size size;
+
+			public Hall(string theLighting="light", Portal portal1, Portal portal2, Size theSize) {
+				portalA = portal1;
+				portalB = portal2;
+				size = theSize;
+				lighting = theLighting;
+			}
+		}
+
+		public struct Size {
+			public int x, y;
+
+			public Size(int xIn, int yIn) {
+				x = xIn;
+				y = yIn;
+			}
 		}
 	}
 }
--- Room.cs
+++ Room.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Generator;
 
 public class Room
 {
