ef3da71ce63b7ef079af54b7cb718a9810c7bf6d
theLads <theladstpk@gmail.com>
2019-10-30 17:11:10 -0700

    Some updates

---
+++ Dungeon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Generator {
+	public class Room {
+    	bool floor;
+	    bool walls;
+    	bool ceiling;
+	    bool floor;
+    	string shape;
+	    string lighting;
+    	// Size size;
+	    // Portal[] portals;
+
+    	public Room() { 
+    	}
+
+	    // Reuturn some sort of graphical layout
+    	void GenerateLayout() {
+
+    	}
+	}
+
+	public struct Portal {
+		public Room roomA, roomB;
+
+		public Portal() {
+			roomA = room1;
+			roomB = room2;	
+		}
+	}
+}
